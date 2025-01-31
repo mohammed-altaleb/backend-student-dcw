342fef724801bd7eea6d862a77c8d35f89280bf4
mohammed-altaleb <medoo-s3ood@hotmail.com>
2025-01-31 14:45:51 +0300

    first commit

---
+++ src/main/java/com/example/backendstudent/BackendStudentApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendstudent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStudentApplication.class, args);
+    }
+
+}
